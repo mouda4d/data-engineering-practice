c344166846160744f57f3d72d8dbd1f4e400c0c2
Mahmoud Ahmed <110298603+mouda4d@users.noreply.github.com>
2024-12-26 17:08:37 +0200

    functional program version one

--- Exercises/Exercise-2/main.py
+++ Exercises/Exercise-2/main.py
@@ -1,11 +1,33 @@
 import requests
-import pandas
-
+import pandas as pd
+import os
 
 def main():
-    # your code here
-    pass
+    ncei_url = 'https://www.ncei.noaa.gov/data/local-climatological-data/access/2021/'
+    dateTimeString = '2024-01-19 10:27'
+    response = requests.get(ncei_url)
+    #print(response.text)
+    with open('NCEI.txt', 'w') as file:
+        file.write(response.text)
+    
+    with open('NCEI.txt', 'r') as file:
+        for line in file.readlines():
+            #print(line)
+            if dateTimeString in line:
+                x = line
+                break
+            
+    #print(x)
+    csv_name = x[17:32]
+    #print(csv_name)
+    full_url = ncei_url + csv_name
+    response = requests.get(full_url)
+    with open(csv_name, 'wb') as file:
+        file.write(response.content)
+    #print(os.listdir('.'))
 
-
+    df = pd.read_csv(csv_name)
+    #print(df)
+    print(df['HourlyDryBulbTemperature'].max())
 if __name__ == "__main__":
     main()
